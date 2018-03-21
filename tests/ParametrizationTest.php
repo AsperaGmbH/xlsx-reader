a40287b507d772a4ce49a36df8057702b208bae7
Rene Fischer <Rene.Fischer@aspera.com>
2018-03-21 16:01:13 +0100

    Fix and refactor TempDir tests * General cleanup testing was moved to TempDirTest. (ParametrizationTest was a suboptimal place for it.) * TempDir option test no longer attempts to create a directory it cannot delete. * Removed heavily OS-dependent umask options. (mkdir mode parameter is ignored under Windows) * Make target temporary directory dependent on PHP configuration, instead of creating temporary files in the execution path.

--- tests/ParametrizationTest.php
+++ tests/ParametrizationTest.php
@@ -11,58 +11,10 @@ use Aspera\Spreadsheet\XLSX\Reader as XLSXReader;
 class ParametrizationTest extends PHPUnitTestCase
 {
     const FILE_PATH = 'input_files/iterator_test.xlsx';
-    const TEMP_DIR_PATH = __DIR__.'/temp_new_folder';
-
-    /** @var XLSXReader */
-    private $reader;
-
-    public function testOptionTempDir()
-    {
-        // create a directory 'read only'
-        @mkdir(self::TEMP_DIR_PATH, 0444);
-
-        $options = array(
-            'TempDir' => self::TEMP_DIR_PATH
-        );
-
-        try {
-            $this->reader = new XLSXReader(self::FILE_PATH, $options);
-            self::fail('[TempDir] parameter check should have failed.');
-        } catch (Exception $e) {
-            // nothing unexpected happened
-            self::assertSame(
-                'XLSXReader: Provided temporary directory ('.self::TEMP_DIR_PATH.') is not writable',
-                $e->getMessage()
-            );
-        }
-    }
-
-    public function testDeletionTemporaryFiles()
-    {
-        // create a directory 'full access'
-        @mkdir(self::TEMP_DIR_PATH);
-
-        $options = array(
-            'TempDir' => self::TEMP_DIR_PATH
-        );
-        $this->reader = new XLSXReader(self::FILE_PATH, $options);
-        // invoke destructor
-        unset($this->reader);
-
-        // after destruction, temporary directory must be totally emptied
-        $folder_scan = @scandir(self::TEMP_DIR_PATH, SCANDIR_SORT_NONE);
-        self::assertCount(2, $folder_scan, 'Folder ['.self::TEMP_DIR_PATH.'] is not empty');
-    }
-
-    public function tearDown()
-    {
-        // cleanup temporary directory
-        if (file_exists(self::TEMP_DIR_PATH)) {
-            @rmdir(self::TEMP_DIR_PATH);
-        }
-    }
 
     /**
+     * Make sure that the SkipEmptyCells option is properly considered by the reader.
+     *
      * @param bool $skip_empty_cells
      * @param int  $exp_num_cols
      *
@@ -76,17 +28,20 @@ class ParametrizationTest extends PHPUnitTestCase
             'SkipEmptyCells' => $skip_empty_cells
         );
 
-        $this->reader = new XLSXReader(self::FILE_PATH, $options);
+        $reader = new XLSXReader(self::FILE_PATH, $options);
 
-        $sheet_index = array_keys($this->reader->getSheets(), 'EmptyCellsSheet');
-        $this->reader->changeSheet($sheet_index[0]);
+        $sheet_index = array_keys($reader->getSheets(), 'EmptyCellsSheet');
+        $reader->changeSheet($sheet_index[0]);
 
-        $current = $this->reader->current();
+        $current = $reader->current();
         $num_cols = count($current);
 
         self::assertEquals($exp_num_cols, $num_cols, 'Number of cells differ');
     }
 
+    /**
+     * @return array
+     */
     public function dataProviderEmptyCells()
     {
         return array(
---
+++ tests/TempDirTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Aspera\Spreadsheet\XLSX\Tests;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Exception;
+use Aspera\Spreadsheet\XLSX\Reader as XLSXReader;
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+/** Test temporary work directory functionality */
+class TempDirTest extends PHPUnitTestCase
+{
+    /** @var string FILE_PATH XLSX File to be used for testing. */
+    const FILE_PATH = 'input_files/iterator_test.xlsx';
+
+    /** @var string TEMP_DIR_NAME Directory name of the temporary work directory. */
+    const TEMP_DIR_NAME = 'temp_new_folder';
+
+    /** @var XLSXReader $reader Reader used by all tests in this test class. */
+    private static $reader;
+
+    /**
+     * Create temporary work directory and initialize the reader, creating temporary files in the process.
+     *
+     * @throws Exception
+     */
+    public function testPrepare()
+    {
+        // Create target directory and configure reader to use it.
+        $temp_dir_path = self::getTempDirPath();
+        @mkdir($temp_dir_path);
+        self::$reader = new XLSXReader(self::FILE_PATH, array(
+            'TempDir' => $temp_dir_path
+        ));
+    }
+
+    /**
+     * Make sure that the TempDir option was properly considered by the reader.
+     *
+     * @depends testPrepare
+     *
+     * @throws Exception
+     */
+    public function testOptionTempDir()
+    {
+        $temp_dir_path = self::getTempDirPath();
+
+        // Check directory contents. The reader should have created files in it.
+        clearstatcache();
+        $file_list = @scandir($temp_dir_path, SCANDIR_SORT_NONE);
+
+        // 2 entries will always exist: ., ..; We expect there to be MORE entries than that, if the reader used the directory.
+        self::assertGreaterThan(2, count($file_list), 'The configured TempDir [' . $temp_dir_path . '] was not used by the reader.');
+    }
+
+    /**
+     * Make sure that all temporary work files are deleted after processing of the target file.
+     *
+     * @depends testOptionTempDir
+     *
+     * @throws Exception
+     */
+    public function testDeletionTemporaryFiles()
+    {
+        $temp_dir_path = self::getTempDirPath();
+
+        // Invoke destructor; This should delete all files from TempDir again.
+        self::$reader = null;
+
+        // After destruction, temporary directory must be totally emptied.
+        clearstatcache();
+        $file_list = @scandir($temp_dir_path, SCANDIR_SORT_NONE);
+
+        // Only 2 entries are expected: ., ..; Any further entries indicate a cleanup failure.
+        self::assertCount(2, $file_list, 'The configured TempDir [' . $temp_dir_path . '] was not emptied.');
+    }
+
+    /**
+     * Remove the temporary work directory.
+     *
+     * @throws Exception
+     */
+    public function testCleanup()
+    {
+        $temp_dir_path = self::getTempDirPath();
+        if (file_exists($temp_dir_path)) {
+            @rmdir($temp_dir_path);
+        }
+    }
+
+    /**
+     * Return temporary work path used for this test. Already includes trailing slash.
+     *
+     * @return string
+     * @throws Exception
+     */
+    private static function getTempDirPath()
+    {
+        $temp_dir_path = sys_get_temp_dir();
+        if (!is_string($temp_dir_path)) {
+            throw new Exception(
+                'Path to temporary work directory could not be determined.'
+                . ' sys_get_temp_dir() returned invalid value.'
+            );
+        }
+        if ($temp_dir_path !== '') {
+            $temp_dir_path = str_replace(chr(92), '/', $temp_dir_path);
+            if (mb_substr($temp_dir_path, -1) !== '/') {
+                $temp_dir_path .= '/';
+            }
+        }
+        return $temp_dir_path . self::TEMP_DIR_NAME;
+    }
+}
